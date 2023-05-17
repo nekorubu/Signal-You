efc40a1af7c62fbdbb1edd9eb11fb8b005546ba3
Johan <johanw@xs4all.nl>
2023-05-17 23:49:11 +0200

    Added extra options
    - Encrypted and plaintext backup / restore.
    - Removed apk expire.
    - Import unencrypted WhatsApp database.
    - Choose between passphrase protection and the Android screenlock.
    - Choice for the backup location: for Android 4 - 10, internal or removable storage; for Android 11+ any directory can be chosen.
    - Set the maptype in the place picker.
    - Option to treat view-once media as normal media.
    - Option to ignore remote deletion.
    - Choose between FCM or websocket notification delivery.
    - Option to delete only the media from a message, not the rest of the message.
    - Removed forward limit.
    - Enabled internal preferences. Use at your own risk.
    - Added options to select who can add you to a group.

--- app/src/main/java/org/thoughtcrime/securesms/keyvalue/MiscellaneousValues.java
+++ app/src/main/java/org/thoughtcrime/securesms/keyvalue/MiscellaneousValues.java
@@ -85,7 +85,7 @@ public final class MiscellaneousValues extends SignalStoreValues {
   }
 
   public boolean isClientDeprecated() {
-    return getBoolean(CLIENT_DEPRECATED, false);
+    return false; // JW
   }
 
   public void markClientDeprecated() {
