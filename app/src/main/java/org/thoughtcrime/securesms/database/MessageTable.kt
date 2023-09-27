e5905431cd9932529cf604bab49e31ba9ded8568
Johan <johanw@xs4all.nl>
2023-09-27 11:25:21 +0200

    Added extra options
    - Encrypted and plaintext backup / restore.
    - Removed apk expire.
    - Import unencrypted WhatsApp database.
    - Choose between passphrase protection and the Android screenlock.
    - Choice for the backup location: for Android 4 - 10, internal or removable storage; for Android 11+ any directory can be chosen.
    - Set the maptype in the place picker.
    - Option to treat view-once media as normal media.
    - Option to ignore remote deletion.
    - Choose between FCM or websocket notification delivery.
    - Option to delete only the media from a message, not the rest of the message.
    - Removed forward limit.
    - Enabled internal preferences. Use at your own risk.
    - Added options to select who can add you to a group.

--- app/src/main/java/org/thoughtcrime/securesms/database/MessageTable.kt
+++ app/src/main/java/org/thoughtcrime/securesms/database/MessageTable.kt
@@ -5433,4 +5433,30 @@ open class MessageTable(context: Context?, databaseHelper: SignalDatabase) : Dat
       }
     }
   }
+
+  //---------------------------------------------------------------------------
+  // JW: Deletes only the attachment for the message, not the message itself.
+  fun deleteAttachmentsOnly(messageId: Long): Boolean {
+    val threadId = getThreadIdForMessage(messageId)
+    val attachmentTable = SignalDatabase.attachments
+    attachmentTable.deleteAttachmentsForMessage(messageId)
+    notifyConversationListeners(threadId)
+    OptimizeMessageSearchIndexJob.enqueue()
+    return true
+  }
+
+  // JW: added functions required for PlaintextBackup
+  fun getMessageCount(): Int {
+    return readableDatabase
+      .select("COUNT(*)")
+      .from(TABLE_NAME)
+      .run()
+      .readToSingleInt()
+  }
+
+  fun getMessages(skip: Int, limit: Int): Cursor {
+    val db = databaseHelper.signalReadableDatabase
+    return db.query(TABLE_NAME, MMS_PROJECTION, null, null, null, null, ID, skip.toString().plus(",").plus(limit.toString()))
+  }
+  //---------------------------------------------------------------------------
 }
