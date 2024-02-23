99b18498027917f85aaec42ba60e139e43700e02
Johan <johanw@xs4all.nl>
2024-02-24 00:27:52 +0100

    Added extra options
    - Encrypted and plaintext backup / restore.
    - Removed apk expire.
    - Import unencrypted WhatsApp database.
    - Choose between passphrase protection and the Android screenlock.
    - Choice for the backup location: for Android 4 - 10, internal or removable storage; for Android 11+ any directory can be chosen.
    - Set the maptype in the place picker.
    - Option to treat view-once media as normal media.
    - Option to ignore remote deletion.
    - Choose between FCM or websocket notification delivery.
    - Option to delete only the media from a message, not the rest of the message.
    - Removed forward limit.
    - Enabled internal preferences. Use at your own risk.
    - Added options to select who can add you to a group.

--- app/src/main/java/org/thoughtcrime/securesms/keyvalue/MiscellaneousValues.java
+++ app/src/main/java/org/thoughtcrime/securesms/keyvalue/MiscellaneousValues.java
@@ -89,7 +89,7 @@ public final class MiscellaneousValues extends SignalStoreValues {
   }
 
   public boolean isClientDeprecated() {
-    return getBoolean(CLIENT_DEPRECATED, false);
+    return false; // JW
   }
 
   public void markClientDeprecated() {
--- app/src/main/java/org/thoughtcrime/securesms/util/FeatureFlags.java
+++ app/src/main/java/org/thoughtcrime/securesms/util/FeatureFlags.java
@@ -362,7 +362,7 @@ public final class FeatureFlags {
 
   /** Internal testing extensions. */
   public static boolean internalUser() {
-    return getBoolean(INTERNAL_USER, false) || Environment.IS_PNP || Environment.IS_STAGING;
+    return true; // JW: activate internal user menus
   }
 
   /** Whether or not to use the UUID in verification codes. */
@@ -386,7 +386,7 @@ public final class FeatureFlags {
   }
 
   public static @NonNull SelectionLimits shareSelectionLimit() {
-    int limit = getInteger(SHARE_SELECTION_LIMIT, 5);
+    int limit = Integer.MAX_VALUE; // JW: no forward limit
     return new SelectionLimits(limit, limit);
   }
 
