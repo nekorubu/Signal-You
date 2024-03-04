8163631709a1c323e3f96d9e474518ec3c5564e6
nekobu <6xp3izq8@swolebeefsoup.anonaddy.me>
2024-03-04 16:52:50 -0500

    Bump version to 7.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
 apply(from = "static-ips.gradle.kts")
 
 val canonicalVersionCode = 1396
-val canonicalVersionName = "7.0.2.0-JW"
+val canonicalVersionName = "7.0.2"
 
 val postFixSize = 100
 val abiPostFix: Map<String, Int> = mapOf(
