6ca28cadaafb852b439c423465e56f979ed68aaf
nekorubu <6xp3izq8@swolebeefsoup.anonaddy.me>
2024-06-13 15:29:01 -0400

    Bumper version to 7.9.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
 apply(from = "static-ips.gradle.kts")
 
 val canonicalVersionCode = 1425
-val canonicalVersionName = "7.9.3.0-JW"
+val canonicalVersionName = "7.9.3"
 
 val postFixSize = 100
 val abiPostFix: Map<String, Int> = mapOf(
