841ad35655f530650272d8d2e509e10474d10139
nekorubu <6xp3izq8@swolebeefsoup.anonaddy.me>
2024-09-26 15:47:08 -0400

    Bump version to 7.18.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
 apply(from = "static-ips.gradle.kts")
 
 val canonicalVersionCode = 1465
-val canonicalVersionName = "7.18.2.0-JW"
+val canonicalVersionName = "7.18.2"
 val currentHotfixVersion = 0
 val maxHotfixVersions = 100
 
