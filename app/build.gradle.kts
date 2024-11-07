0cc3cafda72de23bd53b7439c73cdde4bfe0eaa6
nekorubu <6xp3izq8@swolebeefsoup.anonaddy.me>
2024-11-07 16:11:53 -0500

    Bump version to 7.24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
 apply(from = "static-ips.gradle.kts")
 
 val canonicalVersionCode = 1482
-val canonicalVersionName = "7.24.0.0-JW"
+val canonicalVersionName = "7.24"
 val currentHotfixVersion = 0
 val maxHotfixVersions = 100
 
