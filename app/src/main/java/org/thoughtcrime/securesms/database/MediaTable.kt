03245664da3beb6ef8f5c5997c2049f5cacff3c2
Johan <johanw@xs4all.nl>
2024-12-19 07:26:22 +0100

    Added extra options
    - Encrypted and plaintext (xml) backup / restore.
    - Removed apk expire.
    - Import unencrypted WhatsApp database (this option might be outdated).
    - Choose between passphrase protection and the Android screenlock.
    - Choice for the backup location: for Android 4 - 10, internal or removable storage; for Android 11+ any directory can be chosen.
    - Set the maptype in the place picker.
    - Option to treat view-once media as normal media.
    - Option to ignore remote deletion.
    - Choose between FCM or websocket notification delivery.
    - Option to delete only the media from a message, not the rest of the message. This does not remove the message or media on linked devices.
    - Removed forward limit.
    - Enabled internal preferences. Use at your own risk.
    - Added options to select who can add you to a group.

--- app/src/main/java/org/thoughtcrime/securesms/database/MediaTable.kt
+++ app/src/main/java/org/thoughtcrime/securesms/database/MediaTable.kt
@@ -97,6 +97,8 @@ class MediaTable internal constructor(context: Context?, databaseHelper: SignalD
         GROUP BY ${AttachmentTable.DATA_FILE}
       """
 
+// JW: don't remove link preview media from the list, we want to be able to selectively delete it.
+/*
     private val GALLERY_MEDIA_QUERY = String.format(
       BASE_MEDIA_QUERY,
       """
@@ -116,7 +118,24 @@ class MediaTable internal constructor(context: Context?, databaseHelper: SignalD
         ${MessageTable.LINK_PREVIEWS} IS NULL
       """
     )
+*/
+    private val GALLERY_MEDIA_QUERY = String.format(
+      BASE_MEDIA_QUERY,
+      """
+        ${AttachmentTable.DATA_FILE} IS NOT NULL AND
+        ${AttachmentTable.CONTENT_TYPE} NOT LIKE 'image/svg%' AND 
+        (${AttachmentTable.CONTENT_TYPE} LIKE 'image/%' OR ${AttachmentTable.CONTENT_TYPE} LIKE 'video/%')
+      """
+    )
 
+    private val GALLERY_MEDIA_QUERY_INCLUDING_TEMP_VIDEOS = String.format(
+      BASE_MEDIA_QUERY,
+      """
+        (${AttachmentTable.DATA_FILE} IS NOT NULL OR (${AttachmentTable.CONTENT_TYPE} LIKE 'video/%' AND ${AttachmentTable.REMOTE_INCREMENTAL_DIGEST} IS NOT NULL)) AND
+        ${AttachmentTable.CONTENT_TYPE} NOT LIKE 'image/svg%' AND 
+        (${AttachmentTable.CONTENT_TYPE} LIKE 'image/%' OR ${AttachmentTable.CONTENT_TYPE} LIKE 'video/%')
+      """
+    )
     private val AUDIO_MEDIA_QUERY = String.format(
       BASE_MEDIA_QUERY,
       """
@@ -125,6 +144,8 @@ class MediaTable internal constructor(context: Context?, databaseHelper: SignalD
       """
     )
 
+// JW
+/*
     private val ALL_MEDIA_QUERY = String.format(
       BASE_MEDIA_QUERY,
       """
@@ -133,6 +154,8 @@ class MediaTable internal constructor(context: Context?, databaseHelper: SignalD
         ${MessageTable.LINK_PREVIEWS} IS NULL
       """
     )
+*/
+    private val ALL_MEDIA_QUERY = String.format(BASE_MEDIA_QUERY, "${AttachmentTable.DATA_FILE} IS NOT NULL AND ${AttachmentTable.CONTENT_TYPE} NOT LIKE 'text/x-signal-plain'")
 
     private val DOCUMENT_MEDIA_QUERY = String.format(
       BASE_MEDIA_QUERY,
