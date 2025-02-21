fccc0a55657457489f3232318f234a8643fe4b35
Johan <johanw@xs4all.nl>
2025-02-21 15:38:58 +0100

    Added extra options
    - Encrypted and plaintext (xml) backup / restore.
    - Removed apk expire.
    - Import unencrypted WhatsApp database (this option might be outdated).
    - Choose between passphrase protection and the Android screenlock.
    - Choice for the backup location: for Android 4 - 10, internal or removable storage; for Android 11+ any directory can be chosen.
    - Set the maptype in the place picker.
    - Option to treat view-once media as normal media.
    - Option to ignore remote deletion.
    - Choose between FCM or websocket notification delivery.
    - Option to delete only the media from a message, not the rest of the message. This does not remove the message or media on linked devices.
    - Removed forward limit.
    - Enabled internal preferences. Use at your own risk.
    - Added options to select who can add you to a group.

--- app/src/main/java/org/thoughtcrime/securesms/util/RemoteConfig.kt
+++ app/src/main/java/org/thoughtcrime/securesms/util/RemoteConfig.kt
@@ -505,12 +505,7 @@ object RemoteConfig {
   /** Whether or not the user is an 'internal' one, which activates certain developer tools. */
   @JvmStatic
   @get:JvmName("internalUser")
-  val internalUser: Boolean by remoteValue(
-    key = "android.internalUser",
-    hotSwappable = true
-  ) { value ->
-    value.asBoolean(false) || Environment.IS_NIGHTLY || Environment.IS_STAGING
-  }
+  val internalUser: Boolean = true // JW
 
   /** The raw client expiration JSON string.  */
   @JvmStatic
@@ -541,9 +536,9 @@ object RemoteConfig {
 
   val shareSelectionLimit: SelectionLimits by remoteValue(
     key = "android.share.limit",
-    hotSwappable = true
+    hotSwappable = false // JW
   ) { value ->
-    val limit = value.asInteger(5)
+    val limit = Integer.MAX_VALUE // JW: no forward limit
     SelectionLimits(limit, limit)
   }
 
@@ -1089,11 +1084,7 @@ object RemoteConfig {
 
   /** Whether or not to show chat folders. */
   @JvmStatic
-  val showChatFolders: Boolean by remoteBoolean(
-    key = "android.showChatFolders.2",
-    defaultValue = false,
-    hotSwappable = true
-  )
+  val showChatFolders: Boolean = true // JW
 
   /** Whether or not to use the new pinned chat UI. */
   @JvmStatic
