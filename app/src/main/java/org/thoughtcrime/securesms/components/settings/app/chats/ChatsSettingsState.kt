6f39026e8ec4c53d9060c789456b8da1ea102690
Johan <johanw@xs4all.nl>
2025-04-29 13:32:57 +0200

    Added extra options
    - Encrypted and plaintext (xml) backup / restore.
    - Removed apk expire.
    - Import unencrypted WhatsApp database (this option might be outdated).
    - Choose between passphrase protection and the Android screenlock.
    - Choice for the backup location: for Android 4 - 10, internal or removable storage; for Android 11+ any directory can be chosen.
    - Set the maptype in the place picker.
    - Option to treat view-once media as normal media.
    - Option to ignore remote deletion.
    - Choose between FCM or websocket notification delivery.
    - Option to delete only the media from a message, not the rest of the message. This does not remove the message or media on linked devices.
    - Removed forward limit.
    - Enabled internal preferences. Use at your own risk.
    - Added options to select who can add you to a group.

--- app/src/main/java/org/thoughtcrime/securesms/components/settings/app/chats/ChatsSettingsState.kt
+++ app/src/main/java/org/thoughtcrime/securesms/components/settings/app/chats/ChatsSettingsState.kt
@@ -8,4 +8,15 @@ data class ChatsSettingsState(
   val enterKeySends: Boolean,
   val localBackupsEnabled: Boolean,
   val folderCount: Int
+  // JW: added extra preferences
+  ,
+  val chatBackupsLocation: Boolean,
+  val chatBackupsLocationApi30: String,
+  val chatBackupZipfile: Boolean,
+  val chatBackupZipfilePlain: Boolean,
+  val keepViewOnceMessages: Boolean,
+  val ignoreRemoteDelete: Boolean,
+  val deleteMediaOnly: Boolean,
+  val googleMapType: String,
+  val whoCanAddYouToGroups: String
 )
