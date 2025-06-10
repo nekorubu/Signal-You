f83558238f6a42c553a213179095b4602dca8229
Johan <johanw@xs4all.nl>
2025-06-10 13:09:32 +0200

    Added extra options
    - Encrypted and plaintext (xml) backup / restore.
    - Removed apk expire.
    - Import unencrypted WhatsApp database (this option might be outdated).
    - Choose between passphrase protection and the Android screenlock.
    - Choice for the backup location: for Android 4 - 10, internal or removable storage; for Android 11+ any directory can be chosen.
    - Set the maptype in the place picker.
    - Option to treat view-once media as normal media.
    - Option to ignore remote deletion.
    - Choose between FCM or websocket notification delivery.
    - Option to delete only the media from a message, not the rest of the message. This does not remove the message or media on linked devices.
    - Removed forward limit.
    - Enabled internal preferences. Use at your own risk.
    - Added options to select who can add you to a group.

--- app/src/main/java/org/thoughtcrime/securesms/main/MainToolbar.kt
+++ app/src/main/java/org/thoughtcrime/securesms/main/MainToolbar.kt
@@ -102,6 +102,7 @@ interface MainToolbarCallback {
   fun onCloseActionModeClick()
   fun onSearchQueryUpdated(query: String)
   fun onNotificationProfileTooltipDismissed()
+  fun onImportExportClick() // JW
 
   object Empty : MainToolbarCallback {
     override fun onNewGroupClick() = Unit
@@ -123,6 +124,7 @@ interface MainToolbarCallback {
     override fun onCloseActionModeClick() = Unit
     override fun onSearchQueryUpdated(query: String) = Unit
     override fun onNotificationProfileTooltipDismissed() = Unit
+    override fun onImportExportClick() = Unit // JW
   }
 }
 
@@ -691,6 +693,20 @@ private fun ChatDropdownItems(state: MainToolbarState, callback: MainToolbarCall
       }
     )
   }
+
+  // JW: added
+  DropdownMenus.Item(
+    text = {
+      Text(
+        text = stringResource(R.string.arrays__import_export_orig),
+        style = MaterialTheme.typography.bodyLarge
+      )
+    },
+    onClick = {
+      callback.onImportExportClick()
+      onOptionSelected()
+    }
+  )
 
   DropdownMenus.Item(
     text = {
