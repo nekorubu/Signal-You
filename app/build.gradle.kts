d6e7d97a73f733cda62e806b05ca1755482755e6
nekorubu <6xp3izq8@swolebeefsoup.anonaddy.me>
2025-08-20 12:16:46 -0400

    Bump version to 7.53.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
 apply(from = "static-ips.gradle.kts")
 
 val canonicalVersionCode = 1575
-val canonicalVersionName = "7.53.3.0-JW"
+val canonicalVersionName = "7.53.3"
 val currentHotfixVersion = 0
 val maxHotfixVersions = 100
 
